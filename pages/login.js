cd52932c934fa5e2074ca7a50ebba98e52388a5b
DextrousDavid <aimakhededavido@gmail.com>
2020-07-19 23:23:54 +0100

    work structure built

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+
+const Login = () => (
+  <div>
+    <Navbar />
+    <h1>Welcome to Login page!</h1>
+  </div>
+);
+
+export default Login;
---
+++ pages/register.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar';
+
+const Register = () => (
+  <div>
+    <Navbar />
+    <h1>Welcome to Register page!</h1>
+  </div>
+);
+
+export default Register;
